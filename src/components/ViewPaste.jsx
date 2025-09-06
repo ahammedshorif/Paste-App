81ccf2ab329980affc7a51d988fd52aad83516d0
Ahammed Shorif <ahammedshorif20@gmail.com>
2025-09-07 00:42:19 +0600

    change some design in viewpaste page

--- src/components/ViewPaste.jsx
+++ src/components/ViewPaste.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import toast from 'react-hot-toast';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { Copy } from 'lucide-react';
 
 function ViewPaste() {
    
@@ -15,22 +16,22 @@ function ViewPaste() {
    
   return (
     <div className="">
-        <div className='w-full'>
-           <h1 className='bg-blue-500 p-2 text-2xl font-bold'>Title: {paste.title}</h1>
+        <div className='w-full '>
+           <h1 className='bg-blue-500 p-2 text-2xl font-bold font-serif rounded-t-xl'>Title: {paste.title}</h1>
                                  
            <div>
               <div className='flex justify-end bg-blue-950 p-2'>
                 <button 
-                  className='bg-white text-black p-1 rounded-lg cursor-pointer'
+                  className=' p-1 rounded-lg cursor-pointer'
 
                   onClick={()=>{
                     navigator.clipboard.writeText(paste?.content);
                     toast.success("Copy Successfully")
                   }}
-                >Copy</button>
+                ><Copy size={20} color="#ffffff" strokeWidth={1.25} className='cursor-pointer'/></button>
               </div>
               <textarea 
-              className="w-full rounded-b-xl min-h-[400px] bg-black p-2 border-2 md:min-w-[700px]  md:min-h-[400px]"
+              className="w-full rounded-b-xl min-h-[400px] bg-black p-3 border-2 md:min-w-[700px] font-serif  md:min-h-[400px]"
               name="" 
               id=""
               disabled
